f8030234f7f5cefb88152a59c06c6075623d01eb
Djalma <djalma.ant@gmail.com>
2020-03-31 16:58:19 -0300

    Path and Its Properties

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Course
 {
@@ -6,7 +7,16 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\folderdj\elvis7.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
         }
     }
 }
